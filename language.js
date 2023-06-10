635db5fe3f92dfce5579a886929e58ff1ffd3694
Chathura369 <89286413+Chathura369@users.noreply.github.com>
2023-06-10 08:52:36 +0530

    Update language.js

--- language.js
+++ language.js
@@ -13,7 +13,7 @@ if (fs.existsSync('./language/' + global.LANG + '.json')) {
         chalk.red.bold('You entered an invalid language. English language was chosen.')
     );
 
-    var json = JSON.parse(fs.readFileSync('./language/EN.json'));
+    var json = JSON.parse(fs.readFileSync('./language/SI.json'));
 }
 
 function getString(file) {
